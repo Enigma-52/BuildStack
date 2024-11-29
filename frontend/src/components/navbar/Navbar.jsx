b852ccceba31c1db5513d0434c6604e3ea75d701
Arunabho Bhattacharya <barunabho@gmail.com>
2024-11-29 20:07:32 +0530

    responsive upd

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -10,115 +10,152 @@ import Modal from "./modals/modal";
 import AuthContent from "./auth-content";
 import SearchModal from "./modals/searchModal";
 import SearchContent from "./search-content";
-import { User, Rocket } from "lucide-react";
+import { User, Rocket, X } from "lucide-react";
 import { IoMenuOutline } from "react-icons/io5";
 
 const Navbar = () => {
-  const [authModalVisible, setAuthModalVisible] = useState(false);
-  const [searchModalVisible, setSearchModalVisible] = useState(false);
-  const [isSignedIn, setIsSignedIn] = useState(false);
-  const [scrolled, setScrolled] = useState(false);
+	const [authModalVisible, setAuthModalVisible] = useState(false);
+	const [searchModalVisible, setSearchModalVisible] = useState(false);
+	const [isSignedIn, setIsSignedIn] = useState(false);
+	const [scrolled, setScrolled] = useState(false);
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > 0);
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-  useEffect(() => {
-    // Check if user is signed in by looking for token
-    const token = localStorage.getItem("token");
-    console.log(token);
-    setIsSignedIn(!!token);
-  }, []);
+	useEffect(() => {
+		const handleScroll = () => {
+			setScrolled(window.scrollY > 0);
+		};
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
 
-  const handleButtonClick = () => {
-    setAuthModalVisible(true);
-  };
+	useEffect(() => {
+		// Check if user is signed in by looking for token
+		const token = localStorage.getItem("token");
+		console.log(token);
+		setIsSignedIn(!!token);
+	}, []);
 
-  const handleSearchButtonClick = () => {
-    setSearchModalVisible(true);
-  };
+	const handleButtonClick = () => {
+		setAuthModalVisible(true);
+	};
 
-  const handleProfileClick = () => {
-    window.location.href = "/profile";
-  };
+	const handleSearchButtonClick = () => {
+		setSearchModalVisible(true);
+	};
 
-  useEffect(() => {
-    const handleKeydown = (event) => {
-      if (event.ctrlKey && event.key === "k") {
-        event.preventDefault();
-        setSearchModalVisible(true);
-      }
-    };
+	const handleProfileClick = () => {
+		window.location.href = "/profile";
+	};
 
-    window.addEventListener("keydown", handleKeydown);
-    return () => {
-      window.removeEventListener("keydown", handleKeydown);
-    };
-  }, []);
+	const toggleMobileMenu = () => {
+		setIsMobileMenuOpen(!isMobileMenuOpen);
+	};
 
-  return (
-    <>
-	<nav className={`fixed w-full z-50 transition-all duration-300 ${
-        scrolled ? 'bg-white/80 backdrop-blur-md shadow-lg' : 'bg-transparent'
-      }`}>
-      <div className="border-b py-2 md:py-0 px-4 md:px-6">
-        <div className="flex items-center justify-between my-6">
-          <div className="flex items-center">
-			<IoMenuOutline className="w-8 h-8 text-black mr-4 sm:hidden"/>
-            <div className="relative group">
-              <div className="absolute -inset-1 bg-gradient-to-r from-orange-600 to-orange-400 rounded-full blur opacity-25 group-hover:opacity-75 transition duration-200"></div>
-              <Rocket className="w-8 h-8 text-orange-500 relative" />
-            </div>
-            <span className="mx-1 text-lg font-bold bg-clip-text text-transparent bg-gradient-to-r from-orange-600 to-orange-400 max-sm:hidden">
-              BuildStack
-            </span>
-            {/*<Logo />*/}
-            <div onClick={handleSearchButtonClick}>
-              <Search />
-            </div>
-          </div>
+	useEffect(() => {
+		const handleKeydown = (event) => {
+			if (event.ctrlKey && event.key === "k") {
+				event.preventDefault();
+				setSearchModalVisible(true);
+			}
+		};
 
-          <div className="mx-5 absolute right-1/2 translate-x-1/2 transform-z-10">
-            <Menu />
-          </div>
+		window.addEventListener("keydown", handleKeydown);
+		return () => {
+			window.removeEventListener("keydown", handleKeydown);
+		};
+	}, []);
 
-          <div className="flex items-center mx-4">
-            <button className="group px-6 py-2 mx-4 bg-gradient-to-r from-orange-600 to-orange-400 text-white rounded-full font-medium transition-all duration-200 hover:shadow-lg hover:shadow-orange-500/30">
-              <span className="flex items-center gap-2">Subscribe</span>
-            </button>
+	return (
+		<>
+			<nav
+				className={`fixed w-full z-50 transition-all duration-300 ${scrolled ? 'bg-white/80 backdrop-blur-md shadow-lg' : 'bg-transparent'
+					}`}
+			>
+				<div className={`py-2 md:py-0 px-4 md:px-6 ${isMobileMenuOpen ? 'border-b-0' : 'border-b-2'}`}>
+					<div className="flex items-center justify-between my-6">
+						<div className="flex items-center">
+							<button
+								className="sm:hidden"
+								onClick={toggleMobileMenu}
+							>
+								{isMobileMenuOpen ? (
+									<X className="w-8 h-8 text-black mr-4" />
+								) : (
+									<IoMenuOutline className="w-8 h-8 text-black mr-4" />
+								)}
+							</button>
+							<div className="relative group">
+								<div className="absolute -inset-1 bg-gradient-to-r from-orange-600 to-orange-400 rounded-full blur opacity-25 group-hover:opacity-75 transition duration-200"></div>
+								<Rocket className="w-8 h-8 text-orange-500 relative" />
+							</div>
+							<span className="mx-1 text-lg font-bold bg-clip-text text-transparent bg-gradient-to-r from-orange-600 to-orange-400 max-sm:hidden">
+								BuildStack
+							</span>
+							{/*<Logo />*/}
+							<div onClick={handleSearchButtonClick}>
+								<Search />
+							</div>
+						</div>
 
-            {isSignedIn ? (
-              <Button
-                onClick={handleProfileClick}
-                variant="ghost"
-                className="rounded-full p-2 hover:bg-gray-100"
-              >
-                <User className="h-6 w-6 text-gray-600" />
-              </Button>
-            ) : (
-              <div onClick={handleButtonClick}>
-                <SignInButton />
-              </div>
-            )}
-          </div>
+						<div className="mx-5 absolute right-1/2 translate-x-1/2 transform-z-10">
+							<Menu />
+						</div>
 
-          <Modal visible={authModalVisible} setVisible={setAuthModalVisible}>
-            <AuthContent />
-          </Modal>
-          <SearchModal
-            visible={searchModalVisible}
-            setVisible={setSearchModalVisible}
-          >
-            <SearchContent />
-          </SearchModal>
-        </div>
-      </div>
-	  </nav>
-    </>
-  );
+						<div className="flex items-center mx-4">
+							<button className="group px-6 py-2 mx-4 bg-gradient-to-r from-orange-600 to-orange-400 text-white rounded-full font-medium transition-all duration-300 hover:shadow-lg hover:shadow-orange-500/30">
+								<span className="flex items-center gap-2">Subscribe</span>
+							</button>
+
+							{isSignedIn ? (
+								<Button
+									onClick={handleProfileClick}
+									variant="ghost"
+									className="rounded-full p-2 hover:bg-gray-100"
+								>
+									<User className="h-6 w-6 text-gray-600" />
+								</Button>
+							) : (
+								<div onClick={handleButtonClick}>
+									<SignInButton />
+								</div>
+							)}
+						</div>
+					</div>
+				</div>
+			</nav>
+
+			{/* Mobile Menu */}
+			<div
+				className={`fixed sm:hidden top-0 left-0 w-full h-full bg-white z-40 transition-all duration-300 ${isMobileMenuOpen ? 'opacity-100 visible' : 'opacity-0 invisible'
+					}`}
+					
+			>
+				<div className="h-full flex flex-col justify-center items-center">
+					{/* Add your mobile menu items here */}
+					hello
+					<a href="/" className="text-2xl font-bold mb-4">
+						Home
+					</a>
+					<a href="/about" className="text-2xl font-bold mb-4">
+						About
+					</a>
+					<a href="/contact" className="text-2xl font-bold mb-4">
+						Contact
+					</a>
+				</div>
+			</div>
+
+			<Modal visible={authModalVisible} setVisible={setAuthModalVisible}>
+				<AuthContent />
+			</Modal>
+			<SearchModal
+				visible={searchModalVisible}
+				setVisible={setSearchModalVisible}
+			>
+				<SearchContent />
+			</SearchModal>
+		</>
+	);
 };
 
 export default Navbar;
