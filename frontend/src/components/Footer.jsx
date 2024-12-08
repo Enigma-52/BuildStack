9f71968f92afd9dc47030cae361b1a607e6dc12b
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2024-12-08 17:42:28 +0530

    navbar menu mobile view fixed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Rocket, Linkedin } from "lucide-react";
+import { Rocket } from "lucide-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 function Footer() {
   return (
     <>
