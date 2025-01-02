1c874526adc48c8bd60f0927006d2a383a012fc1
Rohit Singh <rohit.singh.33521@gmail.com>
2025-01-02 11:58:07 +0530

    fix-tests

--- backend/microservices/product-service/tests/product.test.ts
+++ backend/microservices/product-service/tests/product.test.ts
@@ -5,6 +5,6 @@ describe('Health request', () => {
   it('should return HTTP 200 OK for health check', async () => {
     const response = await request('http://localhost:3001').get('/api/health');
     expect(response.status).toBe(200);
-    expect(response.body.status).toBe('OK');
+    expect(response.body.status).toBe('ok');
   });
 });
